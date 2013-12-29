b787bcb3f571a9f3f61a2b35cb94353b8404aeff
Lukas Oppermann <lukas@vea.re>
2013-12-29 12:22:57 +0100

    adding mark theme

--- mark-0.0.1.js
+++ mark-0.0.1.js
@@ -1,7 +1,29 @@
-var editor = ace.edit("mark");
-editor.renderer.setShowGutter(true);
-editor.setShowPrintMargin(false);
-editor.getSession().setUseWrapMode(true);
+nodeList = document.getElementsByClassName('mark');
 
-editor.setTheme("ace/theme/mark");
-editor.getSession().setMode("ace/mode/markdown");
+Array.prototype.slice.call(nodeList,0).forEach(function(editor){
+	var myCodeMirror = CodeMirror.fromTextArea(editor, {
+	  // value: "function myScript(){return 100;}\n",
+		mode: {
+			name: "gfm",
+			highlightFormatting: true
+		},
+		lineNumbers: true,
+		lineWrapping: true,
+		flattenSpans: true,
+		cursorHeight: 0.66,
+		matchBrackets: true,
+		autoCloseBrackets: { pairs: "()[]{}''\"\"", explode: "{}" },
+		matchTags: true,
+		showTrailingSpace: true,
+		autoCloseTags: true,
+		styleSelectedText: true,
+		styleActiveLine: true,
+		placeholder: "",
+      tabMode: 'indent',
+		tabindex: "2",
+		dragDrop: false,
+		extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"},
+		theme: "mark",
+	});
+});
+
